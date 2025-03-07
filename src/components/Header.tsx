3f15f5ae66e0254d81fefb8af7bb5e87ca976054
Vanley Basso <vanley_basso@hotmail.com>
2025-03-07 13:15:37 -0300

    feat: create home page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,40 +11,40 @@ const Header = () => {
         Get 25% OFF on your first order. <a href="#" className="underline">Order Now</a>
       </div>
 
-      
       <nav className="flex justify-between items-center p-4 bg-white shadow-md">
-       
-        <div className="md:hidden ml-4"> 
+        
+        
+        <div className="md:hidden ml-4">
           <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="mt-2">
             {isMenuOpen ? (
-              <X className="w-6 h-6" /> 
+              <X className="w-6 h-6" />
             ) : (
-              <Menu className="w-6 h-6" /> 
+              <Menu className="w-6 h-6" />
             )}
           </button>
         </div>
 
         
-        <div className="hidden md:flex items-center gap-2 ml-[174px]">
+        <div className="flex items-center gap-2">
           <img src="/src/assets/Logomark.png" alt="Logo" className="h-8" />
           <span className="text-lg font-semibold">Ecommerce</span>
         </div>
 
-      
-        <ul className="hidden md:flex gap-6 text-gray-700 flex-grow justify-start ml-20">
+        
+        <ul className="hidden md:flex gap-6 text-gray-700 flex-grow justify-center">
           <li><a href="#" className="hover:text-black">Home</a></li>
           <li><a href="#" className="hover:text-black">Shop</a></li>
           <li><a href="#" className="hover:text-black">About</a></li>
         </ul>
 
-       
-        <div className="flex gap-4 md:mr-[150px]"> 
-          <img src="/src/assets/car.png" alt="Carrinho" className="w-5 h-5 cursor-pointer" /> 
-          <img src="/src/assets/perfil.png" alt="Usuário" className="w-5 h-5 cursor-pointer" /> 
+        
+        <div className="flex gap-4 md:ml-auto">
+          <img src="/src/assets/car.png" alt="Carrinho" className="w-5 h-5 cursor-pointer" />
+          <img src="/src/assets/perfil.png" alt="Usuário" className="w-5 h-5 cursor-pointer" />
         </div>
       </nav>
 
-     
+      
       {isMenuOpen && (
         <ul className="md:hidden flex flex-col items-center bg-white shadow-md py-2">
           <li><a href="#" className="block py-2 text-gray-700 hover:text-black">Home</a></li>
