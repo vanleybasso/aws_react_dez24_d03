b71139532721dfec28c59409c4209e5cff4fcae7
Vanley Basso <vanley_basso@hotmail.com>
2025-03-10 13:08:17 -0300

    feat: create product page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 // src/App.tsx
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Listing from "./pages/Listing"; // Importe a nova página Listing
+import Listing from "./pages/Listing";
+import Product from "./pages/Product"; // Importe a nova página Product
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/listing" element={<Listing />} /> {/* Nova rota */}
+          <Route path="/listing" element={<Listing />} />
+          <Route path="/product" element={<Product />} /> {/* Nova rota */}
         </Routes>
       </main>
     </Router>
